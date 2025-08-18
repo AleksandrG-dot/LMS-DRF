847bbdafe5ce9b9217c2d77a8edddedda2e0d617
Aleksandr Goncharov <potta@inbox.ru>
2025-08-18 12:58:52 +0500

    creating a user model and application materials

--- users/models.py
+++ users/models.py
@@ -1,5 +1,40 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 class User(AbstractUser):
-    pass
+    username = None
+    email = models.EmailField(
+        unique=True, verbose_name="Почта", help_text="Укажите почту"
+    )
+    phone = models.CharField(
+        max_length=18,
+        blank=True,
+        null=True,
+        verbose_name="Телефон",
+        help_text="Укажите номер телефона",
+    )
+    city = models.CharField(
+        max_length=25,
+        blank=True,
+        null=True,
+        verbose_name="Город",
+        help_text="Укажите Ваш город",
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatar",
+        blank=True,
+        null=True,
+        verbose_name="Аватар",
+        help_text="Загрузите аватар",
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return f"e-mail: {self.email}  city: {self.city}  phone: {self.phone}"
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
