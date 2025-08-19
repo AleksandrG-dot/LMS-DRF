cf1cbcf4dc57c5481d90834bcf42a674a3ecf3a5
Aleksandr Goncharov <potta@inbox.ru>
2025-08-20 02:20:47 +0500

    adding generic for lessons. Writing the serializer and paths in urls.py

--- materials/views.py
+++ materials/views.py
@@ -1,9 +1,35 @@
 from rest_framework import viewsets
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, DestroyAPIView, UpdateAPIView
 
-from .models import Course
-from .serializer import CourseSerializer
+from .models import Course, Lesson
+from .serializer import CourseSerializer, LessonSerializer
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+
+class LessonCreateApiView(CreateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonListApiView(ListAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonRetrieveApiView(RetrieveAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonUpdateApiView(UpdateAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+
+class LessonDestroyApiView(DestroyAPIView):
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
