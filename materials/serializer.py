7d3f97222ce93afc68364db59614d2ab8bc7be48
Aleksandr Goncharov <potta@inbox.ru>
2025-08-24 21:25:02 +0500

    Zadanie 3. Vtoroi variant.

--- materials/serializer.py
+++ materials/serializer.py
@@ -15,7 +15,17 @@ class CourseSerializer(serializers.ModelSerializer):
     """Сериализатор для модели курсов"""
 
     lessons_count = serializers.SerializerMethodField(read_only=True)
+
+    # Первый вариант решения задачи 3: Вывод списка уроков для курса.
+    # Вывод с помощью сериализатора для связанной модели
     lessons = LessonSerializer(source="lesson_set", many=True, read_only=True)
+
+    # Второй вариант решения задачи 3: Вывод списка уроков для курса.
+    # Через вычисляемые данные и списком
+    # lessons = serializers.SerializerMethodField()
+    #
+    # def get_lessons(selfs, course):
+    #     return [lesson.title for lesson in Lesson.objects.filter(course=course)]
 
     def get_lessons_count(self, instance):
         return instance.lesson_set.count()
