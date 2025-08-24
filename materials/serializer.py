98894eea50e84740db6e3e6082f41e6d4d2370f7
Aleksandr Goncharov <potta@inbox.ru>
2025-08-24 21:05:28 +0500

    Zadanie 3. Serializator modeli kyrsa vivodit yroki s pomochiy serializatora LessonSerializer

--- materials/serializer.py
+++ materials/serializer.py
@@ -3,22 +3,23 @@ from rest_framework import serializers
 from .models import Course, Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
-    """Сериализатор для модели курсов"""
-
-    lesson_count = serializers.SerializerMethodField(read_only=True)
-
-    def get_lesson_count(self, instance):
-        return instance.lesson_set.count()
-
-    class Meta:
-        model = Course
-        fields = ("id", "title", "preview", "description", "lesson_count")
-
-
 class LessonSerializer(serializers.ModelSerializer):
     """Сериализатор для модели уроков"""
 
     class Meta:
         model = Lesson
         fields = "__all__"
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели курсов"""
+
+    lessons_count = serializers.SerializerMethodField(read_only=True)
+    lessons = LessonSerializer(source="lesson_set", many=True, read_only=True)
+
+    def get_lessons_count(self, instance):
+        return instance.lesson_set.count()
+
+    class Meta:
+        model = Course
+        fields = ("id", "title", "preview", "description", "lessons_count", "lessons")
